928b1740fa1234f48827734bdb19d595bd17ed11
Ovayo <ntebeovayo16@gmail.com>
2026-06-16 00:00:11 +0200

    Ex 2 - FizzBuzz complete

--- ProgramStructure/loops.js
+++ ProgramStructure/loops.js
@@ -1,4 +1,4 @@
-//EX 1
+//EX 1 - Looping a triangle
 let x = 1;
 let hashes = "";
 while(x != 8)
@@ -8,4 +8,25 @@ while(x != 8)
     x++;
 }
 
-//Ex 2
+//Ex 2 - FizzBuzz
+  console.log("FizzBizz");
+for(let i = 1; i < 101; i++)
+{
+    if((i%3==0) && (i%5 == 0))
+    {
+        console.log("FizzBuzz");
+    }
+    else if(i % 3 == 0)
+    {
+        console.log("Fizz");
+    }
+    else if( i % 5 == 0)
+    {
+        console.log("Buzz");
+    }
+    else
+    {
+         console.log(i);
+    }
+   
+}
