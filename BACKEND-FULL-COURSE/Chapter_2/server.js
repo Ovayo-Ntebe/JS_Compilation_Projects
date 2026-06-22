8e4ea2a7d0978963007ee3b1be9a5198d0ef1b1c
Ovayo <ntebeovayo16@gmail.com>
2026-06-22 21:10:30 +0200

    configure nodemon

--- BACKEND-FULL-COURSE/Chapter_2/server.js
+++ BACKEND-FULL-COURSE/Chapter_2/server.js
@@ -1,1 +1,13 @@
 //all app server code here- use express as its easier.. then the http require module
+
+//import the module express
+const express = require('express');
+
+//call express as function to run a app
+const app = express();
+
+//define port num
+const PORT = 3000;
+
+//server listen 
+app.listen(PORT, () => console.log(`Server has started listening on port ${PORT} `))
