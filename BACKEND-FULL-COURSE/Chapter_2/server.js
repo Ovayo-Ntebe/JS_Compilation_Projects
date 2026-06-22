f6ce9d4bb3acffe0c9bee5a600d3297259ba08a7
Ovayo <ntebeovayo16@gmail.com>
2026-06-22 21:50:52 +0200

    add home api

--- BACKEND-FULL-COURSE/Chapter_2/server.js
+++ BACKEND-FULL-COURSE/Chapter_2/server.js
@@ -7,7 +7,21 @@ const express = require('express');
 const app = express();
 
 //define port num
-const PORT = 3000;
+const PORT = 3000; 
+//the address of the server connected to the network is:
+// URL-> http://localhost:3000
+//IP -> 127.0.0.3000
+/*HTTP verbs and routes - need tp configure app for these verbs and routes
+the method informs the nature of the request and the route is a further subdirectory(basically we direct the request to the body of code to repond approriatly
+and these locations or routes are called endpoints) */
+
+app.get('/', (req, res) => { 
+    //this is endpoint 1 - /
+    console.log('yay I hit an endpoint', req.method); //just printing the type of method which is get on consolse
+    res.sendStatus(200); // this returns OK on client side browser
+
+});
+
 
 //server listen 
 app.listen(PORT, () => console.log(`Server has started listening on port ${PORT} `))
