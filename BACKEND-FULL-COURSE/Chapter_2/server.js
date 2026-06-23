b55aa6b27cf64044f4e2fb217c917b608c5bb67d
Ovayo <ntebeovayo16@gmail.com>
2026-06-23 10:46:39 +0200

    added post, delete methods and see whole crud in action using emulator

--- BACKEND-FULL-COURSE/Chapter_2/server.js
+++ BACKEND-FULL-COURSE/Chapter_2/server.js
@@ -16,9 +16,11 @@ and these locations or routes are called endpoints) */
 const PORT = 3000; 
 
 //dummy data for api
-const data = {
-    name: 'OV'
-}
+const data = ["ov"];
+//--Set up middleware- tells server to expect json objects from client
+app.use(express.json()); //where contet type is josn from test.rest
+
+
 /*CRUD-method, create-post, read-get, update-put, delete-delete */
 
 /*---Type 1 -Website endpoints( these endpoints are for sending back html and they typically come when a user enters a url in a browser) */
@@ -34,13 +36,20 @@ app.get('/', (req, res) => {
     <body style = "background:pink; color:black">
         <h1>Body:</h1>
         <p>${JSON.stringify(data)}</p>
+        <a href="/dashboard">Dashboard</a>
     </body>`)
 
 });
 
 app.get('/dashboard', (req, res) => {
     console.log('ohh now hit the /dashboard endpoint');
-    res.send('<h1>dashboard</h1>');
+    res.send(`
+        <body>
+            <h1>dashboard</h1>
+            <a href = "/">home</a>
+        </body>   
+        `
+    );
 })
 
 
@@ -48,6 +57,25 @@ app.get('/dashboard', (req, res) => {
 app.get('/api/data', (req, res) => {
     console.log('this api is for data');
     res.send(data);
+    //if you want can also do res.status(599).send(data) - this will send the status code as well show the data, 599 is just eg
+})
+
+app.post('/api/data', (req,res) => {
+    /*Someone wants to craete a user(eg when they click a sign up button)
+    the user clicks the sign up button after entering therir credentials 
+    and their browser is wired up to send out a nectwork request to the server to handle that action*/
+
+    const newEntry = req.body ;//most req has body
+    console.log(newEntry); //need to include middleware otherwise will show undefined- helps expect json data
+    data.push(newEntry.name)
+    res.sendStatus(201);
+})
+
+
+app.delete('/api/data', (req, res)=>{
+    data.pop();
+    console.log('We deleted the element off the end of the array');
+    res.sendStatus(204); //successful delete
 })
 
 //server listen 
