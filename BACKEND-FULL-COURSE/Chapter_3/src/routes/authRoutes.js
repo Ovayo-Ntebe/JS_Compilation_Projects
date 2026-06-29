1a90702f4a62ac0c64906996d69a2086b3c2d5a6
Ovayo <ntebeovayo16@gmail.com>
2026-06-29 04:23:29 +0200

    auth routes code

--- BACKEND-FULL-COURSE/Chapter_3/src/routes/authRoutes.js
+++ BACKEND-FULL-COURSE/Chapter_3/src/routes/authRoutes.js
@@ -11,9 +11,16 @@ import db from '../db.js';
 const router = express.Router();
 
 //Register a new user endpoint /auth/register
-router.post('/register', (req, res) => {});
+router.post('/register', (req, res) => {
+    //get the info from the body of the request. 
+    const body = req.body; //can use object desctructing as well const{username, password} = req.body
+    const username = body.username;
+    const password = body.password;
+    console.log(username, password);
+    res.sendStatus(201); //created status
+});
 
-router.post('/Login', (req, res)=> {});
+router.post('/login', (req, res)=> {});
 
 
 
